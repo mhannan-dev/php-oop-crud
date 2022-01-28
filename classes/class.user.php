f56a689d00f1ef36c968b41e96bf446068808712
codepundit1 <jahid35-266@diu.edu.bd>
2022-01-28 20:40:28 +0600

    simple update

--- classes/class.user.php
+++ classes/class.user.php
@@ -1,6 +1,6 @@
 <?php
 
-include "db_config.php";
+include './config/db_config.php';
 
 class User{
 
--- index.php
+++ index.php
@@ -1,49 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP oop login reg system</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!-- custome css -->
-
-
-</head>
-
-<body>
-
-
-
-
-
-
-    <!-- nav bar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid nav-color">
-            <a class="navbar-brand" href="index.php">Login & Register system using PHP </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.php">Login</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="registration.php">Register</a>
-                    </li>
-
-                </ul>
-
-            </div>
-        </div>
-    </nav>
-
-    <!-- navbar end -->
-
+<?php
+     
+     include("./includes/navbar.php");
+?>
 
 
 </body>
@@ -60,34 +19,6 @@
 
 
 
-<!-- footer -->
-
-<footer class="bg-light text-center text-lg-start pt-5">
-
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2022 Copyright: Jahid Hasan
-        <a class="text-primary" href="https://www.google.com.com/">Google</a>
-    </div>
-
-</footer>
-<!-- end footer -->
-
-
-
-
-
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-
-<script>
-    // $(function(){
-    //     alert('working')
-    // }
-    // )
-</script>
-
-</body>
-
-</html>
+<?php
+    include("./includes/footer.php");
+?>
